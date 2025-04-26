ef5a4468bf8375119ab623e41877e93804b07d17
xbenitogranell <42180472+xbenitogranell@users.noreply.github.com>
2025-04-26 07:35:07 +0000

    Deploying to gh-pages from @ xbenitogranell/xbenitogranell.github.io@48c49ee80be43f554fd507a616e4e21276b26ba6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -444,9 +444,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-decision",
+          title: 'DECISION',
+          description: "Advancing quantitative paleoecology to guide socio-ecological system resilience of a Mediterranean delta to global change",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
