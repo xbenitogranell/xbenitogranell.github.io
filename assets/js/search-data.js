33f91ac06eb6e2a91aa215fd1917016b4f1a6a6f
xbenitogranell <42180472+xbenitogranell@users.noreply.github.com>
2025-04-27 10:27:11 +0000

    Deploying to gh-pages from @ xbenitogranell/xbenitogranell.github.io@806ace3807027e3a409f3516bdddb78062c77202 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
+  },{id: "nav-research",
+          title: "research",
           description: "",
           section: "Navigation",
           handler: () => {
